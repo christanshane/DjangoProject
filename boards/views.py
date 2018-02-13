1cc23217a6a3c9aec5f73463e241352ee042116b
cosmoTechnologist <cosmoTechnologist@yahoo.com>
2018-02-14 05:05:05 +0800

    adding new topic now requires login, also uses actual starter name instead of first user in DB

--- boards/views.py
+++ boards/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.http import Http404
 from django.contrib.auth.models import User
 from .forms import NewTopicForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+
 
 from .models import Board, Topic, Post
 # Create your views here.
@@ -14,20 +15,20 @@ def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
 
+@login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    user = User.objects.first()  # TODO: get the currently logged in user
     if request.method == 'POST':
         form = NewTopicForm(request.POST)
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.starter = user
+            topic.starter = request.user  # <- here
             topic.save()
-            post = Post.objects.create(
+            Post.objects.create(
                 message=form.cleaned_data.get('message'),
                 topic=topic,
-                created_by=user
+                created_by=request.user  # <- and here
             )
             return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
     else:
